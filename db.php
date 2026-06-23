bf9f749d69bba09751de3cc581207e3ea7c7725d
smileyzn <smileyznl@gmail.com>
2026-06-23 10:14:38 -0300

    kkk

--- db.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('SG_DB_HOST', 'localhost');
-define('SG_DB_PORT', '3306');
-define('SG_DB_NAME', 'pugbr_app');
-define('SG_DB_USER', 'pugbr_app');
-define('SG_DB_PASS', '7jRqOW73IRo9MOL');
